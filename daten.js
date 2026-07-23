18b8e2d42dd2e67cff044975c4c435f8dfbb3bd1
Florany <Bisaflora@protonmail.com>
2026-07-23 20:00:00 +0200

    NeueBlöcke

--- daten.js
+++ daten.js
@@ -1,40 +1,140 @@
 const sammlung = [
+
     {
-        titel: "Diddl is Back",
+        datum: "Oktober 2025",
+
+        a5: {
+            normale: [],
+            duft: []
+        },
+
+        a6: {
+            normale: [],
+            duft: [],
+            sonder: []
+        }
+    },
+
+    {
+        datum: "MID Kollektion - April 2026",
+
+        a5: {
+            normale: [
+                "MA501.jpg",
+                "MA502.jpg",
+                "MA503.jpg"
+            ],
+            duft: []
+        },
+
+        a6: {
+            normale: [
+                "MA601.jpg",
+                "MA602.jpg",
+                "MA603.jpg",
+                "MA604.jpg",
+                "MA605.jpg",
+                "MA606.jpg"
+            ],
+            duft: [],
+            sonder: []
+        }
+    },
+
+    {
+        datum: "Herzkollektion - April 2026",
+
+        a4: {
+            normale: [
+                "HA401.jpg",
+                "HA402.jpg"
+            ]
+        },
+
+        a5: {
+            normale: [
+                "HA501.jpg",
+                "HA502.jpg",
+                "HA503.jpg",
+                "HA504.jpg"
+            ],
+            duft: []
+        },
+
+        a6: {
+            normale: [
+                "HA601.jpg",
+                "HA602.jpg",
+                "HA603.jpg",
+                "HA604.jpg"
+            ],
+            duft: [],
+            sonder: []
+        }
+    },
+
+    {
+        datum: "Back to School - Mai 2026",
+
+        a5: {
+            normale: [
+                "BA501.jpg",
+                "BA502.jpg",
+                "BA503.jpg"
+            ],
+            duft: []
+        },
+
+        a6: {
+            normale: [
+                "BA601.jpg",
+                "BA602.jpg",
+                "BA603.jpg",
+                "BA604.jpg",
+                "BA605.jpg",
+                "BA606.jpg"
+            ],
+            duft: [],
+            sonder: []
+        }
+    },
+
+    {
         datum: "Juli 2026",
 
         a5: {
             normale: [
-                "A501",
-                "A502"
+                "A501.png",
+                "A502.png"
             ],
 
             duft: [
-                "A503",
-                "A504",
-                "A505",
-                "A506"
+                "A503.png",
+                "A504.png",
+                "A505.png",
+                "A506.png"
             ]
         },
 
         a6: {
             normale: [
-                "A601",
-                "A602"
+                "A601.png",
+                "A602.png"
             ],
 
             duft: [
-                "A603",
-                "A604",
-                "A605",
-                "A606"
+                "A603.png",
+                "A604.png",
+                "A605.png",
+                "A606.png"
             ],
 
             sonder: [
-                "A607",
-                "A608",
-                "A609"
+                "A607.png",
+                "A608.png",
+                "A609.png"
             ]
         }
     }
+
 ];
